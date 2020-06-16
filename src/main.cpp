bf2051e328a14cbf6a79bd0f5bd03cb203827764
LeNguyenHoangNhan <public.hoangnhanlenguyen@gmail.com>
2020-06-16 20:10:18 +0700

    Refactor code

--- src/main.cpp
+++ src/main.cpp
@@ -24,10 +24,10 @@ double setPoint = 3500.0;
 double pos = 0.0;
 double adjust = 0.0;
 
-double initialBaseSpeed = 150;
+const double initialBaseSpeed = 150;
 
 // Line follow base speed
-double baseSpeed = 170.0;
+const double baseSpeed = 170.0;
 
 // use these with baseSpeed = 170
 double Kp = 0.065;
@@ -35,78 +35,42 @@ double Ki = 0.0;
 double Kd = 0.0013;
 
 uint64_t looopStartMillis = 0;
+bool lineFollowInitialized = false;
+uint8_t status = 0;
+bool tunelPassed = false;
+bool ramUp = true;
+double speed = initialBaseSpeed;
+uint64_t switchMillis = 0;
+uint64_t currentMillis = 0;
 
 QTRSensors qtr;
 PID pid(&pos, &adjust, &setPoint, Kp, Ki, Kd, DIRECT);
 
-bool lineFollowInitialized = false;
-bool inRamUp = true;
-
-uint8_t status = 0;
-bool tunelPassed = false;
-
-bool ramUp = true;
-double speed = initialBaseSpeed;
-uint64_t lastCountMillis = 0;
-
-uint64_t switchMillis = 0;
-
 uint32_t checkDistance() {
-    digitalWrite(SONAR_TRIG_PIN, 1);
+    PORTC |= (1 << PC0);
     delayMicroseconds(5);
-    digitalWrite(SONAR_TRIG_PIN, 0);
-    uint64_t dur = pulseIn(A1, HIGH);
-    uint32_t dis = dur / 2 / 29.412;
-    return dis;
+    PORTC &= ~(1 << PC0);
+    uint8_t oldSREG = SREG;
+    cli();
+    uint64_t dur = pulseIn(A1, HIGH, 1000000UL);
+    sei();
+    SREG = oldSREG;
+    return dur / 2 / 29.412;
 }
 
 inline void adjustCarTunnel() {
     Motor::setSpeed(255, 10);
 }
-
-#if (!FLAG_USE_NEW_CALIBRATE_PROCEDURE)
-
-void calibrateSensor() {
-    digitalWrite(LED_BUILTIN, HIGH);  // turn on Arduino's LED to indicate we are in calibration mode
-
-    Motor::setSpeed(60, 60);
-    for (uint16_t i = 0; i < 400; i++) {
-        qtr.calibrate();
-        if (i < 50) {
-            Motor::setDir(Motor::motorDir::forward, Motor::motorDir::reverse);
-        } else if (i > 50 && i < 100) {
-            Motor::setDir(Motor::motorDir::reverse, Motor::motorDir::forward);
-        } else if (i > 100 && i < 150) {
-            Motor::setDir(Motor::motorDir::forward, Motor::motorDir::reverse);
-        } else if (i > 150 && i < 200) {
-            Motor::setDir(Motor::motorDir::reverse, Motor::motorDir::forward);
-        } else if (i > 200 && i < 250) {
-            Motor::setDir(Motor::motorDir::forward, Motor::motorDir::reverse);
-        } else if (i > 250 && i < 300) {
-            Motor::setDir(Motor::motorDir::reverse, Motor::motorDir::forward);
-        } else if (i > 300 && i < 350) {
-            Motor::setDir(Motor::motorDir::forward, Motor::motorDir::reverse);
-        } else if (i > 350 && i < 400) {
-            Motor::setDir(Motor::motorDir::reverse, Motor::motorDir::forward);
-        }
-    }
-    Motor::setSpeed(0, 0);
-    Motor::setDir(Motor::motorDir::forward, Motor::motorDir::forward);
-    digitalWrite(LED_BUILTIN, LOW);  // turn off Arduino's LED to indicate we are through with calibration
-}
-#else
 void calibrateSensor() {
     LED::turnOn();
     Motor::setDir(Motor::motorDir::forward, Motor::motorDir::reverse);
-    Motor::setSpeed(90, 90);
+    Motor::setSpeed(50, 50);
     for (uint16_t i = 0; i < 400; i++)
         qtr.calibrate();
     Motor::setSpeed(0, 0);
     Motor::setDir(Motor::motorDir::forward, Motor::motorDir::forward);
     LED::turnOff();
 }
-
-#endif
 
 void initLineFollowMode() {
     pid.SetTunings(Kp, Ki, Kd);
@@ -125,7 +89,7 @@ int main() {
     pinMode(SONAR_ECHO_PIN, INPUT);
     pinMode(START_BUTTON_PIN, INPUT_PULLUP);
 
-    // config indicator led
+    // status indicator led
     pinMode(LED_BUILTIN, OUTPUT);
 
     // configure the line sensors
@@ -133,6 +97,7 @@ int main() {
     qtr.setSensorPins(sensorPins, sensorCount);
     qtr.setEmitterPin(2);
     qtr.setNonDimmable();
+    qtr.setTimeout(2000);
     delay(500);
 
     // Init motor (timer,...)
@@ -150,9 +115,10 @@ int main() {
     };
     looopStartMillis = millis();
     for (;;) {
+        currentMillis = millis();
         if (status == 0) {  // if status = 0 aka following line
             if (ramUp) {
-                if (millis() - looopStartMillis > 750) {
+                if (currentMillis - looopStartMillis > 750) {
                     pid.SetOutputLimits((double)-baseSpeed, (double)baseSpeed);
                     speed = baseSpeed;
                     ramUp = false;
@@ -162,16 +128,14 @@ int main() {
             pos = qtr.readLineBlack(sensorValues);
             // if detect line
             if ((sensorValues[4] > 800 && sensorValues[3] > 800 && (sensorValues[2] > 800 || sensorValues[5] > 800))) {
-                if (millis() - switchMillis > 500) {
+                if (currentMillis - switchMillis > 500) {
                     // doesn't switch when ram up to avoid the START arrow
-                    if (!ramUp) {
-                        if (millis() - looopStartMillis >= 7000) {
-                            if (!tunelPassed)
-                                status = 1;
-                            else  // jump to stop
-                                status = 100;
-                            switchMillis = millis();
-                        }
+                    if (currentMillis - looopStartMillis >= 7000) {
+                        if (!tunelPassed)
+                            status = 1;
+                        else  // jump to stop
+                            status = 100;
+                        switchMillis = currentMillis;
                     }
                 }
             }
@@ -199,7 +163,7 @@ int main() {
 
         } else if (status == 1) {  // aka tunnel mode
             qtr.readCalibrated(sensorValues);
-            if (millis() - switchMillis > 500 && (sensorValues[0] > 700 || sensorValues[1] > 700 || sensorValues[2] > 700 || sensorValues[3] > 700 || sensorValues[4] > 700 || sensorValues[5] > 700 || sensorValues[6] > 700 || sensorValues[7] > 700)) {
+            if (currentMillis - switchMillis > 500 && (sensorValues[0] > 700 || sensorValues[1] > 700 || sensorValues[2] > 700 || sensorValues[3] > 700 || sensorValues[4] > 700 || sensorValues[5] > 700 || sensorValues[6] > 700 || sensorValues[7] > 700)) {
                 status = 0;
                 tunelPassed = true;
                 switchMillis = millis();
@@ -211,16 +175,14 @@ int main() {
 
         } else if (status == 100) {
             Motor::setSpeed(200, 100);
-            delay(200);
+            delay(100);
             Motor::setSpeed(0, 0);
             status = 200;
         } else if (status == 200) {
             if (!digitalRead(START_BUTTON_PIN)) {
                 status = 0;
                 tunelPassed = false;
-#if (FLAG_USE_RAMUP)
                 ramUp = true;
-#endif
                 looopStartMillis = millis();
             };
         }
